f65ad7f2ebd703acc1f4abeb01e2dff06f9c611a
jeongyy123 <jeongyy123@kakao.com>
2023-10-21 21:20:57 +0900

    while문 수정

--- 02.js
+++ 02.js
@@ -1,13 +1,15 @@
-//while은 시간이 오래걸린다.
-// function solution(arr) {
-//     var answer = 0;
-//     var i = 0;
-//     while (i < arr.length) {
-//         answer += arr[i];
-//     }
-//     return answer;
-// }
+//while문 이용
+function solution(arr) {
+    var answer = 0;
+    var i = 0;
+    while (i < arr.length) {
+        answer += arr[i];
+        i++
+    }
+    return answer;
+}
 
+//for문 이용
 // function solution(arr) {
 //     var answer = 0;
 //     for (var i=0; i < arr.length; i++) {
@@ -15,11 +17,6 @@
 //     }
 //     return answer/arr.length;
 // }
-
-//use reduce
-function solution(arr) {
-    return arr.reduce((acc, cur) => acc + cur)
-}
 
 var arr1 = [1, 2, 3, 4];
 var arr2 = [5, 5];
