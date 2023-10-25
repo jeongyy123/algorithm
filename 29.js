abf72357f2088201865d33f8a98973cfb864c6f9
jeongyy123 <jeongyy123@kakao.com>
2023-10-25 21:54:56 +0900

    N개의 최소공배수 문제 추가(실패)

--- 29.js
+++ 29.js
@@ -34,36 +34,14 @@
 //     return answer;
 // }
 
-//다시 시작
-//answer[0] = 최대공약수 answer[1]= 최소공배수
-//최대공약수 = 두 수를 동시에 나눌 수 있는 수 중 가장 큰 수
-//  최소공배수 = 두 수의 공통된 배수 중 가장 작은 수
-//  최소공배수 = 두 자연수의 곱 / 최대공약수
-
-// function solution(n, m) {
-//     let answer = [];
-//     let gcd = 1;
-//     for (let i = 1; i < Math.max(n,m);i++){
-//         if (n % i ===0 && m % i === 0) {
-//             gcd = i
-//         }
-//     }
-//     let lcm = (n*m) / gcd
-
-//     answer[0] = gcd;
-//     answer[1] = lcm;
-//     return answer;
-// }
-
 function solution(n, m) {
     //최대 공약수 => answer[0] = 최대공약수
     //유클리드 호제법 함수 정의
-    //2개의 자연수 또는 정식(整式)의 최대공약수를 구하는 알고리즘의 하나이다. 호제법이란 말은 두 수가 서로(互) 상대방 수를 나누어(除)서 결국 원하는 수를 얻는 알고리
     let getAlgorithm = function(n, m) {
         if (Math.max(n,m) % Math.min(n,m) === 0){
-            return m;
+            return Math.min(n,m);
         } else {
-            return getAlgorithm(m, n % m);
+            return getAlgorithm(Math.min(n,m), Math.max(n,m) % Math.min(n,m));
         }
     }; 
 
@@ -76,15 +54,43 @@ function solution(n, m) {
     return answer;
 }
 
-
 let n1 = 2304;
 let n2 = 2;
 let n3 = 20;
 
 let m1 = 1440;
-let m2 = 5;
+let m2 = 7;
 let m3 = 30;
 
 console.log(solution(n1, m1)); //[ 288, 11520 ]
 console.log(solution(n2, m2)); //[ 1, 10 ]
 console.log(solution(n3, m3)); //[ 10, 60 ]
+
+
+//------------------------------------------------------
+//https://school.programmers.co.kr/learn/courses/30/lessons/12953
+//N개의 최소공배수
+//두 수를 먼저 최대공약수를 구한 후 구해진 최대공약수와 남은 수의 최대공약수를 구하는...
+// function solution(arr) {
+//     let getAlgorithm = function(arr) {
+//         if (Math.max(...arr) % Math.min(...arr) === 0){
+//             return Math.min(...arr);
+//         } else {
+//             return getAlgorithm(Math.min(...arr), Math.max(...arr) % Math.min(...arr));
+//         }
+//     }; 
+
+//     let answer = 0;
+//     // let gcd = 1
+//     let gcd = getAlgorithm(...arr)
+
+//     answer = ( arr.reduce((acc,cur) => acc * cur, 1) ) / gcd;
+//     return answer;
+// }
+
+
+// const arr1 = [2,6,8,14];
+// const arr2 = [1,2,3];
+
+// console.log(solution(arr1)); //168  
+// console.log(solution(arr2)); //6
