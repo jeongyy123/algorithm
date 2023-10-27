7a0fde57ea946e1d88852d007ee4bdda985721b0
jeongyy123 <jeongyy123@kakao.com>
2023-10-28 08:34:26 +0900

    update(미해결)

--- 둘만의암호.js
+++ 둘만의암호.js
@@ -4,18 +4,27 @@
 function solution(s, skip, index) {
     //1. s, skip를 아스키코드로 변경 => index만큼 이동
     let arrS = s.split("").map((value) => value.charCodeAt()) //[ 97, 117, 107, 107, 115 ]
-    let arrSkip = skip.split("").map((value) => value.charCodeAt())
-
+    let arrSkip = skip.split("").map((value) => value.charCodeAt()) //[ 119, 98, 113, 100 ]
     //2. 아스키코드를 변경하되 소문자를 벗어나는 것 숫자 조절
     //소문자 아스키코드 범위: 97(a)~122(z)
-    let arrSSkip = arrS.map(val => val+ index > 122 ? val+index - 26 : val+index) //[ 102, 122, 112, 112, 120 ]
+    let arrSSkip = arrS.map(val => val + index > 122 ? val + index - 26 : val + index) //[ 102, 122, 112, 112, 120 ]
 
-    //3. s ~ arrSSkip 범위 안에 skip(짧을수있어) 있으면 index+갯수
+    //3. arrS ~ arrSSkip 범위 안에 skip 번호가 있으면 count 배열에 하나씩 넣어 // 여기서부터 문제
+    let count = new Array(arrS.length).fill(0)
     for (let i = 0; i < arrS.length; i++) {
-        console.log(`arrS: ${arrS[i]}, arrSkip: ${arrSkip[i]}, arrSSkip: ${arrSSkip[i]}`)
-        if( arrS[i] < arrSkip[i] && arrSkip[i] <= arrSSkip[i]) {
-            console.log("잘되나요?")
+        arrSkip.find(num => (arrS[i] <= num && num <= arrSSkip[i]) ? count[i] += 1 : 0)
+    }
+    //4.count를 arrSSkip에서 빼서 아크시코드 -> 문자 
+    let answer = ""
+    for (let i = 0; i < arrSSkip.length; i++) {
+        if ( 96 > arrSSkip[i]-count[i]) {
+            answer = String.fromCharCode(arrSSkip[i]-count[i]+26);
+        } else if ( arrSSkip[i]-count[i] > 122){
+            answer = String.fromCharCode(arrSSkip[i]-count[i]-26);
+        } else {
+            answer = String.fromCharCode(arrSSkip[i]-count[i]);
         }
+        console.log(answer)
     }
 }
 
